9fd888663225de619d29ecc4dd6bab69664f4d9c
mariannelena <73900073+mariannelena@users.noreply.github.com>
2020-12-08 17:24:38 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,14 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Heart)
+    Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 0)
+    basic.pause(5000)
+    Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 0)
+    basic.pause(2500)
+    Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Right, 0)
+    basic.pause(1000)
+    Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 0)
+    basic.pause(1000)
+    Kitronik_Move_Motor.stop()
 })
 basic.forever(function () {
 	
